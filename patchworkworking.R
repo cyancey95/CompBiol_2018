d289455ddcfd7e9b45a05059987abbbbb0660989
Colleen Yancey <35500806+cyancey95@users.noreply.github.com>
2018-03-21 13:10:33 -0400

    changes

---
+++ patchworkworking.R
@@ -0,0 +1,14 @@
+library(ggplot2)
+install.packages("ggplot2")
+library(ggplot2)
+p1<- qplot(runif(100))
+p1
+p2 <-qplot(rnorm(100))
+p2
+library(patchwork)
+install_github("patchwork")
+install.packages("devtools")
+install_github("patchwork")
+library(devtools)
+install_github("patchwork")
+
